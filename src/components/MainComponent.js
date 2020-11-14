673b7322355cf1fe1b20e034c45cabcd01bfdadb
Nitin N <nitinnarayanan77@gmail.com>
2020-11-14 09:13:55 +0530

    controlled form

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import { COMMENTS } from '../shared/comments';
 import { LEADERS } from '../shared/leaders';
 import { PROMOTIONS } from '../shared/promotions';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import About from './AboutComponent';
  
 class Main extends Component {
 
@@ -23,6 +24,7 @@ class Main extends Component {
       leaders: LEADERS,
     };
   }
+  
 
 
   render(){
@@ -33,13 +35,23 @@ class Main extends Component {
         leader={this.state.leaders.filter((leader) => leader.featured)[0]} />
       );
     }
+
+    const DishWithId = ({match}) => {
+      return(
+        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+        comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}/>
+      );
+    }
+
     return (
       <div>
         <Header />
         <Switch>
           <Route path="/home" component={HomePage} />
           <Route exact path="/menu" component={ () => <Menu dishes={this.state.dishes} />} />
+          <Route path="/menu/:dishId" component={DishWithId} />
           <Route path="/contactus" component={Contact} />
+          <Route path="/aboutus" component={ () => <About leaders={this.state.leaders} /> } />
           <Redirect to="/home" />
         </Switch>
         <Footer />
